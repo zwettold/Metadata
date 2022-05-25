5fd87c005acec8511361ce91247f48c810ac37ff
zwett <hello@zwett.co>
2022-05-25 22:40:26 +0200

    Introduce `Metadata` type

--- Sources/Metadata/Metadata.swift
+++ Sources/Metadata/Metadata.swift
@@ -1,2 +1,27 @@
-// Hi there! 👋
-// Glad you're here. Let's figure out the design this library. 🦧
+import Foundation
+
+/// A representation of a website's metadata.
+///
+/// > Important: Temporarily, ``Metadata`` contains only the `title` of the website.
+///              This will be addressed and expanded once the metadata extraction is
+///              properly implemented.
+public struct Metadata: Sendable {
+    /// The title of the website.
+    public let title: String?
+
+    /// Creates a metadata description with the given title.
+    ///
+    /// - Parameter title: The title of the website.
+    @inlinable
+    public init(title: String? = nil) {
+        self.title = title
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension Metadata: CustomStringConvertible {
+    public var description: String {
+        "Metadata(title: \(title ?? "nil"))"
+    }
+}
